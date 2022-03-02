e7b7e453106068d8e1ce0834bc11d7913cda90eb
shameemm <mandayappuramshameem@gmail.com>
2022-03-02 21:47:02 +0530

    models.py modified

--- diary/models.py
+++ diary/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+class Diary(models.Model):
+    title = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+
+    
